a24c3ee84a1d1050470253247231940a19731e74
alittlebitofit <alittlebitofit@git.com>
2024-07-27 11:44:44 +0530

    Practical1c done - Student id, name, course, and dob.
    1. Working with basic C# and ASP .NET
    c. Create an application that receives the (Student Id, Student Name, Course Name, Date of Birth) information from a set of students. The application should also display the information of all the students once the data entered.

---
+++ Practical1c/Practical1c/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Practical1c.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Student Id"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Student Name"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Course Name"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Date of Birth"></asp:Label>
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Display" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="OK" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Practical1c/Practical1c/WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical1c
+{
+
+    struct Student
+    {
+        public string id, name, cname, dob;
+    }
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static Student[] students = new Student[2];
+        static int i = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Student s = new Student();
+
+            s.id = TextBox1.Text;
+            TextBox1.Text = "";
+            
+            s.name = TextBox2.Text;
+            TextBox2.Text = "";
+            
+            s.cname = TextBox3.Text;
+            TextBox3.Text = "";
+            
+            s.dob = TextBox4.Text;
+            TextBox4.Text = "";
+
+            students[i] = s;
+            i++;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Response.Write("Student: " + j + "<br>");
+                Response.Write("Student ID: " + students[j].id + "<br>");
+                Response.Write("Student Name: " + students[j].name + "<br>");
+                Response.Write("Student Course Name: " + students[j].cname + "<br>");
+                Response.Write("Student Date of Birth: " + students[j].dob + "<br>");
+            }
+        }
+    }
+}
