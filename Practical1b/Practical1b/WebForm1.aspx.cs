7248412abfd3200e4da310fbc6996353ceb8b2dd
alittlebitofit <alittlebitofit@git.com>
2024-07-27 11:22:04 +0530

    Practical1b done - String operation
    1. Working with basic C# and ASP .NET
    b. Create an application to demonstrate string operations

---
+++ Practical1b/Practical1b/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical1b
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string str1 = TextBox1.Text;
+
+            string[] words = str1.Split(' ');
+
+            foreach(string word in words)
+            {
+                TextBox2.Text += word + "\n";
+            }
+        }
+    }
+}
