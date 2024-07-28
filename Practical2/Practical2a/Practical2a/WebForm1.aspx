bdb464058ca072c10b725191934ee65f3c74a18a
alittlebitofit <alittlebitofit@git.com>
2024-07-28 16:58:18 +0530

    Practical2a done - Factorial, Money, Quadratic, Temperature
    2. Working with Object Oriented C# and ASP .NET
       a. Create simple application to perform following operations
    	i. Finding factorial Value
    	ii. Money Conversion
    	iii. Quadratic Equation
    	iv. Temperature Conversion

---
+++ Practical2/Practical2a/Practical2a/WebForm1.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Practical2a.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="FactorialLabel" runat="server" Text="Number"></asp:Label>
+            <asp:TextBox ID="FactorialInput" runat="server" TextMode="Number"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Factorial" />
+            <br />
+            <asp:Label ID="FactorialOutput" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="MoneyLabel" runat="server" Text="Enter Money"></asp:Label>
+            <asp:TextBox ID="MoneyInput" runat="server" TextMode="Number"></asp:TextBox>
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+                <asp:ListItem Value="USDINR" Selected="True">USD to INR</asp:ListItem>
+                <asp:ListItem Value="INRUSD">INR to USD</asp:ListItem>
+            </asp:RadioButtonList>
+            <br />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Convert" />
+            <br />
+            <asp:Label ID="MoneyOutput" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Enter Coefficients"></asp:Label>
+            <br />
+            <asp:TextBox ID="aTerm" runat="server"></asp:TextBox>
+            <asp:Label ID="Label2" runat="server" Text="x^2 + "></asp:Label>
+            <asp:TextBox ID="bTerm" runat="server"></asp:TextBox>
+            <asp:Label ID="Label3" runat="server" Text="x +"></asp:Label>
+            <asp:TextBox ID="cTerm" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Solve" />
+            <br />
+            <asp:Label ID="QuadraticSolution" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Enter Temperature"></asp:Label>
+&nbsp;<asp:TextBox ID="TemperatureInput" runat="server" TextMode="Number"></asp:TextBox>
+            <br />
+            <asp:RadioButtonList ID="TemperaturRadioList" runat="server">
+                <asp:ListItem Selected="True" Value="CtoF">C to F</asp:ListItem>
+                <asp:ListItem Value="FtoC">F to C</asp:ListItem>
+            </asp:RadioButtonList>
+            <br />
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Convert" />
+            <br />
+            <asp:Label ID="TemperatureOutput" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Practical2/Practical2a/Practical2a/WebForm1.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical2a
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ulong? num = null;
+            try
+            {
+                num = uint.Parse(FactorialInput.Text);
+            }
+            catch (Exception)
+            {
+                FactorialOutput.Text = "Invalid Output";
+            }
+
+            if (num.HasValue)
+            {
+                ulong? factorial = 1;
+
+                for (ulong? i = num; i > 1; i--)
+                {
+                    factorial *= i;
+                }
+
+                FactorialOutput.Text = $"Factorial of {num} is {factorial}";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            double money = double.Parse(MoneyInput.Text);
+            double result;
+
+
+            if (RadioButtonList1.SelectedValue == "USDINR")
+            {
+                result = money * 83.72;
+                MoneyOutput.Text = $"{money} USD = {result} INR";
+            }
+            else
+            {
+                result = money / 83.72;
+                MoneyOutput.Text = $"{money} INR = {result} USD";
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(aTerm.Text);
+
+            if (a != 0)
+            {
+
+                double b = double.Parse(bTerm.Text);
+                double c = double.Parse(cTerm.Text);
+
+                double sqrtTerm = (b * b) - (4 * a * c);
+                double sqrt = Math.Sqrt(sqrtTerm);
+
+                if (double.IsNaN(sqrt))
+                {
+                    QuadraticSolution.Text = "Given quadratic equation has complex roots";
+                }
+                else
+                {
+                    double root1 = (b + sqrt) / (2 * a);
+                    double root2 = (b - sqrt) / (2 * a);
+
+                    if (root1 == root2)
+                    {
+                        QuadraticSolution.Text = $"Given quadratic quation has real and equal roots: {root1}";
+                    }
+                    else
+                    {
+                        QuadraticSolution.Text = $"Given quadratic quation has real and different roots: {root1} and {root2}";
+                    }
+
+                }
+            }
+            else
+            {
+                QuadraticSolution.Text = "Invalid value for coefficient of x^2";
+            }
+
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            double temperature = double.Parse(TemperatureInput.Text);
+
+            //F = (9/5)C + 32
+            //°C = (°F - 32) × 5/9
+            
+            if (TemperaturRadioList.SelectedValue == "CtoF")
+            {
+                double fahrenheit = (9.0 / 5.0) * temperature + 32;
+                TemperatureOutput.Text = $"{temperature} °C = {fahrenheit} °F";
+            }
+            else
+            {
+                double celsius = (temperature - 32) * (5.0 / 9.0);
+                TemperatureOutput.Text = $"{temperature} °F = {celsius} °C";
+            }
+
+        }
+    }
+}
