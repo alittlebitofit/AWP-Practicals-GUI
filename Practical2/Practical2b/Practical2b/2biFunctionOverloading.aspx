2567454b9075104144e209713ff4c4713d319326
alittlebitofit <alittlebitofit@git.com>
2024-07-31 14:01:09 +0530

    miscellaneous

---
+++ Practical2/Practical2b/Practical2b/2biFunctionOverloading.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="2biFunctionOverloading.aspx.cs" Inherits="Practical2b.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-left: 40px">
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Function Overloading" OnClick="Button1_Click" />
+            <br />
+            <br />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Single Inheritance" />
+            <br />
+            <br />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="MultiLevel Inheritance" />
+            <br />
+            <br />
+            <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Hierarchical Inheritance" />
+            <br />
+            <br />
+            <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="Constructor Overloading" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Practical2/Practical2b/Practical2b/2biFunctionOverloading.aspx.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+
+// Single Inheritance - START
+class Parent
+{
+    internal int d;
+    internal string BaseMethod()
+    {
+        return "This is BaseMethod.";
+    }
+}
+
+class Child : Parent
+{
+    internal string DerivedMethod()
+    {
+        return "This is DerivedMethod.";
+    }
+}
+// Single Inheritance - END
+
+
+// Multilevel Inheritance - START
+class A
+{
+    internal string GrandParentShow()
+    {
+        return "Grandparent class";
+    }
+}
+
+class B : A
+{
+    internal string ParentShow()
+    {
+        return "Parent class";
+    }
+}
+
+class C : B
+{
+    internal string ChildShow()
+    {
+        return "Child class";
+    }
+}
+// Multilevel Inheritance - END
+
+
+
+// Hierarchical Inheritance - START
+class OneParent
+{
+    internal string ShowParent()
+    {
+        return "Parent";
+    }
+}
+
+class FirstChild : OneParent
+{
+    internal string ShowFirstChild()
+    {
+        return "First Child";
+    }
+}
+class SecondChild : OneParent
+{
+    internal string ShowSecondChild()
+    {
+        return "Second Child";
+    }
+}
+// Hierarchical Inheritance - END
+
+
+
+class ConstructorOverloading
+{
+    internal ConstructorOverloading()
+    {
+        Console.Write("Default Constructor called");
+    }
+
+    internal ConstructorOverloading(string s)
+    {
+        Console.Write("Parameterized Constructor called with the parameter: " + s);
+    }
+}
+
+
+
+class FunctionOverloading
+{
+    internal int Sum(int a, int b)
+    {
+        return a + b;
+    }
+    internal int Sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+    internal float Sum(float a, float b)
+    {
+        return a + b;
+    }
+    internal float Sum(float a, float b, float c)
+    {
+        return a + b + c;
+    }
+
+}
+
+
+namespace Practical2b
+{
+
+
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        FunctionOverloading mo;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            mo = new FunctionOverloading();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write($"{mo.Sum(10, 20)}<br>");
+            Response.Write($"{mo.Sum(10, 20, 30)}<br>");
+            Response.Write($"{mo.Sum(23.1f, 3.0f)}<br>");
+            Response.Write($"{mo.Sum(10.34f, 20.8f, 4.6f)}<br>");
+
+            //Response.Redirect("WebForm2.aspx");
+            //Server.Transfer("WebForm2.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Parent p = new Parent();
+            Child c = new Child();
+
+            Response.Write("Calling from Parent class' object: " + p.BaseMethod());
+            Response.Write($"<br>Calling from Child class' object: {c.BaseMethod()}, {c.DerivedMethod()}<br>");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            C c = new C();
+            Response.Write(c.GrandParentShow() + "<br>");
+            Response.Write(c.ParentShow() + "<br>");
+            Response.Write(c.ChildShow() + "<br>");
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            OneParent oneP = new OneParent();
+            FirstChild fc = new FirstChild();
+            SecondChild sc = new SecondChild();
+
+            Response.Write($"Calling Parent from Parent object: {oneP.ShowParent()}<br>");
+            Response.Write($"Calling Parent from FirstChild object: {fc.ShowParent()}<br>");
+            Response.Write($"Calling FirstChild from FirstChild object: {fc.ShowFirstChild()}<br>");
+            Response.Write($"Calling Parent from SecondChild object: {sc.ShowParent()}<br>");
+            Response.Write($"Calling SecondChild from SecondChild object: {sc.ShowSecondChild()}<br>");
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            new ConstructorOverloading();
+            new ConstructorOverloading("Hello");
+        }
+    }
+}
