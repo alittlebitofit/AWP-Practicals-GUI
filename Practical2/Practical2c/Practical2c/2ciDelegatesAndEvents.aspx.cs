e760332be4aff5f844c222f7970bafa7c583a2c0
alittlebitofit <alittlebitofit@git.com>
2024-07-31 14:26:15 +0530

    Practical2c done - Delegates and Events; Exception handling
    2. Working with Object Oriented C# and ASP .NET
        c. Create simple application to demonstrate use of following concepts
            i. Using Delegates and events
            ii. Exception handling

---
+++ Practical2/Practical2c/Practical2c/2ciDelegatesAndEvents.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical2c
+{
+    public partial class DelegatesAndEvents : System.Web.UI.Page
+    {
+
+        public delegate void SimpleDelegate();
+        public void CallingFunction()
+        {
+            Response.Write("First Function Called...<br>");
+        }
+
+        public void SecondFunction()
+        {
+            Response.Write("Second Function Called...<br>");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SimpleDelegate sd = new SimpleDelegate(CallingFunction);
+            sd();
+
+            sd += new SimpleDelegate(SecondFunction);
+            sd();
+        }
+    }
+}
---
+++ Practical2/Practical2c/Practical2c/2ciiExceptionHandling.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+class NegativeException : Exception
+{
+    public NegativeException(string msg) : base(msg)
+    {
+
+    }
+}
+
+namespace Practical2c
+{
+    public partial class _2ciiExceptionHandling : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int num;
+
+            try
+            {
+                num = int.Parse(TextBox1.Text);
+                if (num < 0) throw new NegativeException("Negative number");
+                else Response.Write("Positive number");
+            }
+            catch(NegativeException ne)
+            {
+                Response.Write(ne.Message);
+            }
+        }
+    }
+}
